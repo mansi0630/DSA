a407c822dfa7e041556803f49431cbd10cd3e255
Mansi Soni <136592082+mansi0630@users.noreply.github.com>
2026-09-25 16:39:59 +0530

    Time: 22 ms (18.54%), Space: 98.3 MB (6.09%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        solve(1, n, k, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    void solve(int start, int n, int k,
+               List<Integer> list, List<List<Integer>> ans) {
+
+        if (list.size() == k) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            list.add(i);
+            solve(i + 1, n, k, list, ans); // next number
+            list.remove(list.size() - 1);
+        }
+    }
+}
